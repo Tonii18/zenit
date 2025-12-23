b5aaaecda8bf50e5683b0ad1164e0322630a52a1
Tonii18 <ajsanrosal@gmail.com>
2025-12-23 14:23:26 +0100

    Registration screen and splash screen

--- lib/views/register/register_screen.dart
+++ lib/views/register/register_screen.dart
@@ -2,43 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:zenit/config/app_colors.dart';
 import 'package:zenit/config/measures.dart';
-import 'package:zenit/views/register/register_screen.dart';
 import 'package:zenit/widgets/button.dart';
 import 'package:zenit/widgets/text_form_field.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
 
     final size = Measures.size(context);
     final width = size.width;
     final scale = Measures.scale(context);
-
+    
     return Scaffold(
+
       resizeToAvoidBottomInset: true,
-      
+
       body: Center(
         child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(vertical: scale * 35, horizontal: scale * 35),
+          padding: EdgeInsets.symmetric(vertical: 35 * scale, horizontal: 35 * scale),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
 
             children: [
-              
-              SvgPicture.asset('assets/images/login-vector.svg', height: scale * 200, width: scale * 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    }, 
+                    icon: Icon(Icons.arrow_back, size: scale * 45, color: AppColors.mainGreen,)
+                  )
+                ],
+              ),
+
+              SvgPicture.asset('assets/images/register-vector.svg', height: scale * 200, width: scale * 100),
 
               SizedBox(height: scale * 30),
 
               Text(
-                'Inicia sesion en tu cuenta',
+                'Crea tu cuenta de Zenit',
                 textAlign: TextAlign.start,
                 style: TextStyle(
                   fontSize: scale * 30,
@@ -64,12 +76,38 @@ class _LoginScreenState extends State<LoginScreen> {
 
               CustomeFormField(
                 width: width, 
+                text: 'Nombre de usuario', 
+                textColor: AppColors.lightGrey, 
+                backgroundColor: Color.fromRGBO(243, 245, 247, 1), 
+                iconColor: AppColors.darkerGrey, 
+                focusedOutlinedBorder: AppColors.mainGreen, 
+                isPassword: false, 
+                icon: Icons.account_circle
+              ),
+
+              SizedBox(height: 10 * scale),
+
+              CustomeFormField(
+                width: width, 
+                text: 'Número de teléfono', 
+                textColor: AppColors.lightGrey, 
+                backgroundColor: Color.fromRGBO(243, 245, 247, 1), 
+                iconColor: AppColors.darkerGrey, 
+                focusedOutlinedBorder: AppColors.mainGreen, 
+                isPassword: false, 
+                icon: Icons.phone
+              ),
+
+              SizedBox(height: 10 * scale),
+
+              CustomeFormField(
+                width: width, 
                 text: 'Contraseña', 
                 textColor: AppColors.lightGrey, 
                 backgroundColor: Color.fromRGBO(243, 245, 247, 1), 
                 iconColor: AppColors.darkerGrey, 
                 focusedOutlinedBorder: AppColors.mainGreen, 
-                isPassword: true, 
+                isPassword: false, 
                 icon: Icons.lock
               ),
 
@@ -81,34 +119,18 @@ class _LoginScreenState extends State<LoginScreen> {
                 backgroundColor: AppColors.mainGreen,
                 borderColor: Colors.transparent,
                 borderRadius: 50, 
-                text: 'Iniciar sesión', 
+                text: 'Registrarme', 
                 textColor: AppColors.white, 
                 fontSize: 22 * scale, 
                 fontWeight: FontWeight.w800,
                 onPressed: (){},
               ),
 
-              SizedBox(height: 10 * scale),
-
-              CustomeButton(
-                width: width, 
-                height: 70 * scale, 
-                backgroundColor: AppColors.white,
-                borderColor: AppColors.mainGreen,
-                borderRadius: 50, 
-                text: 'Registrarme', 
-                textColor: AppColors.mainGreen, 
-                fontSize: 22 * scale, 
-                fontWeight: FontWeight.w800,
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()),);
-                },
-              ),
-
             ],
           ),
         ),
       ),
+
     );
   }
 }
