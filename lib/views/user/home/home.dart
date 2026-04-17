6806760ecf941df73522373a468205a6b930795a
Tonii18 <ajsanrosal@gmail.com>
2026-04-17 09:05:44 +0300

    Greeting widget done and working

--- lib/views/user/home/home.dart
+++ lib/views/user/home/home.dart
@@ -15,7 +15,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   String? _name;
   bool _isLoading = true;
 
@@ -35,12 +34,14 @@ class _HomeState extends State<Home> {
       _isLoading = false;
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final size = Measures.size(context);
     final width = size.width;
     final scale = Measures.scale(context);
+
+    // Gradient for welcoming message
 
     final Shader linearGradient = LinearGradient(
       begin: Alignment.centerLeft,
@@ -52,6 +53,18 @@ class _HomeState extends State<Home> {
       ],
       stops: [0.3, 0.6, 0.9],
     ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+
+    // Gradients for day reminder
+
+    final weatherGradient = LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: <Color>[
+        AppColors.mainGreen,
+        AppColors.mainPink,
+        AppColors.mainOrange,
+      ],
+    );
 
     return Scaffold(
       resizeToAvoidBottomInset: true,
@@ -66,9 +79,9 @@ class _HomeState extends State<Home> {
             mainAxisAlignment: MainAxisAlignment.center,
 
             children: [
-              // ==========
-              // HEADER CONTAINER
-              // ==========
+              /**
+               * Header container
+               */
               Container(
                 height: scale * 140,
                 decoration: BoxDecoration(
@@ -111,9 +124,9 @@ class _HomeState extends State<Home> {
 
               SizedBox(height: scale * 20),
 
-              // ==========
-              // PEDOMETER CONTAINER
-              // ==========
+              /**
+               * Pedometer container
+               */
               Container(
                 height: scale * 350,
                 decoration: BoxDecoration(
@@ -124,9 +137,9 @@ class _HomeState extends State<Home> {
 
               SizedBox(height: scale * 20),
 
-              // ==========
-              // WEEK DAYS CONTAINER
-              // ==========
+              /**
+               * Weekdays container
+               */
               Container(
                 height: scale * 60,
                 decoration: BoxDecoration(
@@ -134,10 +147,145 @@ class _HomeState extends State<Home> {
                   borderRadius: BorderRadius.circular(15),
                 ),
               ),
+
+              SizedBox(height: scale * 20),
+
+              /**
+               * Weather and Journal buttons
+               */
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: scale * 90,
+                      decoration: BoxDecoration(
+                        gradient: getGradient(),
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColors.darkerGrey,
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(top: 12, left: 12, child: getIcon()),
+                          Positioned(bottom: 12, right: 12, child: Text(getGreeting(), style: TextStyle(color: AppColors.white),))
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(width: scale * 20),
+
+                  Expanded(
+                    child: Container(
+                      height: scale * 90,
+                      decoration: BoxDecoration(
+                        color: AppColors.white,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  // Function to get a gradient for each moment of the day
+
+  LinearGradient getGradient() {
+    final hour = DateTime.now().hour;
+
+    // Sunrise
+    if (hour >= 5 && hour < 9) {
+      return const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromRGBO(255, 209, 102, 1),
+          Color.fromRGBO(255, 153, 102, 1),
+          Color.fromRGBO(255, 94, 98, 1),
+          Color.fromRGBO(112, 76, 141, 1),
+        ],
+      );
+    }
+    // Noon
+    else if (hour >= 9 && hour < 18) {
+      return const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromRGBO(0, 119, 182, 1),
+          Color.fromRGBO(0, 180, 216, 1),
+          Color.fromRGBO(202, 240, 248, 1),
+          Color.fromRGBO(144, 224, 239, 1),
+          Color.fromRGBO(255, 249, 196, 1),
+        ],
+      );
+    }
+    // Sunset
+    else if (hour >= 18 && hour < 21) {
+      return const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromRGBO(43, 27, 61, 1),
+          Color.fromRGBO(111, 50, 121, 1),
+          Color.fromRGBO(216, 58, 86, 1),
+          Color.fromRGBO(255, 123, 84, 1),
+          Color.fromRGBO(255, 178, 107, 1),
+        ],
+      );
+    }
+    // Night
+    else {
+      return const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromRGBO(11, 14, 20, 1),
+          Color.fromRGBO(22, 27, 34, 1),
+          Color.fromRGBO(36, 52, 71, 1),
+          Color.fromRGBO(61, 78, 123, 1),
+          Color.fromRGBO(142, 148, 178, 1),
+        ],
+      );
+    }
+  }
+
+  // Function to get the emoji which represents the moment of the day
+
+  Icon getIcon() {
+    final hour = DateTime.now().hour;
+
+    if (hour < 18) {
+      return Icon(Icons.sunny, color: Colors.white);
+    } else if (hour < 21) {
+      return Icon(Icons.sunny_snowing, color: Colors.white);
+    } else {
+      return Icon(Icons.nightlight_round, color: Colors.white);
+    }
+  }
+
+  // Function to get the matching greeting according to the time of the day
+
+  String getGreeting() {
+    final hour = DateTime.now().hour;
+
+    if (hour < 12) {
+      return 'Buenos días';
+    } else if (hour < 20) {
+      return 'Buenas tardes';
+    } else {
+      return 'Buenas noches';
+    }
   }
 }
